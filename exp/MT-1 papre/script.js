01649a61cb62f9ba4c68421350b9e823fa648e9f
mahima0148 <mahimapatel654@gmail.com>
2026-06-20 13:56:37 +0530

    mt 1 sol

---
+++ exp/MT-1 papre/script.js
@@ -0,0 +1,10 @@
+function checkNumber() {
+  let number = document.getElementById("num").value;
+  if (number > 0) {
+    document.getElementById("result").innerHTML = "The number is Positive";
+  } else if (number < 0) {
+    document.getElementById("result").innerHTML = "The number is Negative";
+  } else {
+    document.getElementById("result").innerHTML = "The number is Zero";
+  }
+}
